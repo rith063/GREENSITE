bf73824fb343f14e887f2ba81e522077a1a6534e
anomali-channel <m-15205519@moe-dl.edu.my>
2026-06-30 13:52:27 +0800

    Refactor price list and improve calculator functionality

--- script.js
+++ script.js
@@ -1,1280 +1,209 @@
-body{
+// ======================
+// LIVE PRICE
+// ======================
+
+const prices = {
+
+  botol_plastik:0.10,
+  kadbod:0.15,
+  tin_aluminium:0.10,
+  tembaga:2.50,
+  besi:0.30,
+  surat_khabar:0.15,
+  majalah:0.12,
+  botol_kaca:0.25,
+  minyak_masak:2.00,
+  pvc:0.20,
+
+  telefon_lama:5.00,
+  laptop_lama:8.00,
+  tv_lama:4.00,
+  bateri_kereta:9.50,
+  aircond:6.00,
+  mesin_basuh:5.50,
+  peti_ais:7.00,
+  tayar:3.00,
+  cpu_lengkap:5,
+  wayar:1
+
+};
+
+// AUTO DROPDOWN
+
+let select =
+document.getElementById("material");
+
+Object.keys(prices).forEach(item=>{
+
+  let option =
+  document.createElement("option");
+
+  option.value = item;
+  option.text =
+  item.replaceAll("_"," ").toUpperCase();
+
+  select.appendChild(option);
+
+});
 
 
+// ======================
+// CALCULATOR
+// ======================
 
-margin:0;
+function calculate(){
 
+  let item =
+  document.getElementById(
+    "material"
+  ).value;
 
+  let qty =
+  document.getElementById(
+    "qty"
+  ).value;
 
-font-family:Segoe UI;
+  let total =
+  prices[item] * qty;
 
+  document.getElementById(
+    "result"
+  ).innerText =
+  "💰 RM " +
+  total.toFixed(2);
 
+  if(total >= 50){
 
+    document.getElementById(
+      "motivation"
+    ).innerText =
+    "🔥 Hebat! Anda bantu bumi!";
 
+  }else if(total >= 20){
 
-background:
+    document.getElementById(
+      "motivation"
+    ).innerText =
+    "🌱 Bagus! Teruskan recycle!";
 
-url("https://chat.google.com/u/0/api/get_attachment_url?url_type=FIFE_URL&content_type=image%2Fpng&attachment_token=AOo0EEXBhX9nw7oQ94O3oumG8Lbh47ZSNgqeLhBpJBnFU9fNMx6CuLjtSOmAd%2Fm8Kau7%2B9c1RhREMt%2F9vycEffO5wn92Ho%2FG7ValLmgbZMMvaWEQfy7l13v%2BIRGw%2Bv%2B7yOrQmPg3rUXJ76LEjOEpJXaFEhJOhkOvgwJWhElGkRN7YQtDWOY8X4Qh3z4v7yhCf9wdMstA9mPPHX2vX8lNm8MFcYtryugauCDiC8sFW%2BpJIIvf%2FluGqlmwrmtu25E0IakGpo6elnN%2BChCDw2gFrysz6LKKxa4KOlECnfrnyIrXA2i4wunWAx0t03b9Po8W5mJKSKFK5W1j6nEjq13IrCxuAK%2Bm14AjkU6%2F4%2FrC2SjhA2t2TOiXbJ9evM%2Btj3PK2HlaxUAJnn2hkTyy6u0uKCQl89fBP%2FaXr5N7AwpF0SGmJupFuaMBeo%2FLTSvGMDVJSVfBNQ%2B6s3ZdRdZ%2Bi%2BbCiKjj0DJOLlC4%2BPg59g4XgDfYYQr0qoULu6LRN9%2FDk0C%2B3hg7JSfOCs1Z2M0qtJR5VE9mQqqIRirhf2ijPTQhHUrVpoptY3C8AZXgqjPL6iUHLXuUMrBCGW2MmXhOIIwPa7Xg4J4tOUPV5PwudqD%2FHLBLdQWnBZE5Lz%2BneFunjNbiNQy7n9rcqS1UllFYa74tDY1YfAMpOhPVDjNIhw3hgLhLFqU%3D&allow_caching=true&sz=w512")
+  }else{
 
-
-
-no-repeat center center fixed;
-
-
-
-
-
-background-size:cover;
-
-
-
+    document.getElementById(
+      "motivation"
+    ).innerText =
+    "♻️ Setiap usaha bermakna!";
+  }
 
 }
 
+// ======================
+// MAP REAL LOCATION
+// ======================
 
+function findLocation(){
 
+  if(!navigator.geolocation){
+    alert("Browser tidak support location");
+    return;
+  }
 
+  navigator.geolocation.getCurrentPosition(function(pos){
 
+    let lat = pos.coords.latitude;
+    let lon = pos.coords.longitude;
 
+    document.getElementById("map").innerHTML = "";
 
-body::before{
+    let map = L.map('map').setView([lat,lon],15);
 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
 
+    // USER MARKER
+    L.marker([lat,lon])
+      .addTo(map)
+      .bindPopup("📍 Lokasi anda")
+      .openPopup();
 
-content:"";
+    // REAL RECYCLE QUERY
 
+   // REAL RECYCLE QUERY 10KM
 
+let query = `
 
-position:fixed;
+[out:json];
 
+(
 
+node["amenity"="recycling"](around:30000,${lat},${lon});
 
-inset:0;
+way["amenity"="recycling"](around:30000,${lat},${lon});
 
+relation["amenity"="recycling"](around:30000,${lat},${lon});
 
+node["amenity"="waste_transfer_station"](around:30000,${lat},${lon});
 
-background:rgba(0,0,0,0.20);
+way["amenity"="waste_transfer_station"](around:30000,${lat},${lon});
 
+node["shop"="scrap_yard"](around:30000,${lat},${lon});
 
+way["shop"="scrap_yard"](around:30000,${lat},${lon});
 
-z-index:-1;
+);
 
+out center;
 
+`;
 
-}
+    let url =
+    "https://overpass-api.de/api/interpreter?data=" +
+    encodeURIComponent(query);
 
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
 
+      let elements = data.elements;
 
+      if(!elements.length){
+        alert("Tiada pusat recycle dijumpai");
+        return;
+      }
 
+      elements.forEach(place => {
 
+        let pLat =
+        place.lat || place.center?.lat;
 
+        let pLon =
+        place.lon || place.center?.lon;
 
+        let name =
+        place.tags?.name ||
+        "♻️ Recycling Center";
 
+        let marker =
+        L.marker([pLat,pLon]).addTo(map);
 
-header{
+        marker.bindPopup(`
+          <b>${name}</b><br>
+          Klik untuk navigate
+        `);
 
+        marker.on("click", function(){
 
+          window.open(
+            `https://www.google.com/maps/dir/?api=1&destination=${pLat},${pLon}`,
+            "_blank"
+          );
 
-text-align:center;
+        });
 
+      });
 
+    });
 
-padding:50px;
-
-
-
-color:#06402a;
-  
-
-
-
-}
-
-
-
-
-
-
-
-header h1{
-
-
-
-font-size:50px;
-font-family:Arial, Sans serif
-
- 
-  
-
-
-
-}
-
-
-
-
-
-
-
-nav{
-
-
-
-margin-top:20px;
-
-
-
-}
-
-
-
-
-
-
-
-nav a{
-
-
-
-background:white;
-
-
-
-padding:12px 18px;
-
-
-
-margin:5px;
-width: 300px;
-height: 100px;
-border-style: solid;
-border-color: #06402a;
-
-
-border-radius:30px;
-
-
-
-text-decoration:none;
-
-
-
-color:#0b4d2b;
-
-
-
-font-weight:bold;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-section{
-
-
-
-padding:60px 20px;
-
-
-
-text-align:center;
-
-
-
-color:white;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-/* HARGA */
-
-
-
-
-
-.grid{
-
-
-
-display:grid;
-
-
-
-grid-template-columns:
-
-
-
-repeat(auto-fit,minmax(220px,1fr));
-
-
-
-gap:20px;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-.card{
-
-
-
-position:relative;
-
-
-
-background:rgba(255,255,255,0.93);
-
-
-
-color:#0b4d2b;
-
-
-
-padding:20px;
-
-
-
-border-radius:20px;
-
-
-
-overflow:hidden;
-
-
-
-transition:0.3s;
-
-
-
-box-shadow:-10px 10px rgb(6,64,42)
-
-
-
-}
-
-
-
-
-
-
-
-.card:hover{
-
-
-
-transform:translateY(-5px);
-
-
-
-}
-
-
-
-
-
-
-
-.card span{
-
-
-
-font-size:20px;
-
-
-
-font-weight:bold;
-
-
-
-}
-
-
-
-
-
-
-
-.up{
-
-
-
-color:green;
-
-
-
-}
-
-
-
-
-
-
-
-.down{
-
-
-
-color:red;
-
-
-
-}
-
-
-
-
-
-
-
-.stable{
-
-
-
-color:orange;
-
-
-
-}
-
-
-
-
-
-
-
-.criteria{
-
-
-
-position:absolute;
-
-
-
-bottom:-100%;
-
-
-
-left:0;
-
-
-
-width:100%;
-
-
-
-
-
-background:rgba(0,70,30,0.95);
-
-
-
-color:white;
-
-
-
-padding:15px;
-
-
-
-
-
-transition:0.4s;
-
-
-
-}
-
-
-
-
-
-
-
-.card:hover .criteria{
-
-
-
-bottom:0;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-/* KALKULATOR */
-
-
-
-
-
-.box{
-
-
-
-
-
-width:320px;
-
-
-
-margin:auto;
-
-
-
-
-
-
-background:rgba(255,255,255,0.92);
-
-
-
-
-
-padding:25px;
-
-
-width: 300px;
-height: 150px;
-border-style: solid;
-border-color: #06402a;
-
-
-
-
-border-radius:20px;
-
-
-
-
-
-color:black;
-
-
-
-}
-
-
-
-
-
-
-
-.box select,
-
-
-
-.box input{
-
-
-
-
-
-width:100%;
-
-
-
-
-
-padding:12px;
-
-
-
-
-
-margin:10px 0;
-
-
-
-
-
-border-radius:10px;
-
-
-
-
-
-border:none;
-
-
-
-}
-
-
-
-
-
-
-
-button{
-
-
-
-
-
-width:100%;
-
-
-
-
-
-padding:12px;
-
-
-
-
-
-background:#0b4b2b;
-
-
-
-
-
-color:white;
-
-
-
-
-
-border:none;
-
-
-
-
-
-border-radius:10px;
-
-
-
-
-
-cursor:pointer;
-
-
-
-}
-
-
-
-
-
-
-
-button:hover{
-
-
-
-
-
-background:#119c56;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-/* MAP */
-
-
-
-
-
-#map{
-
-
-
-
-
-height:450px;
-
-
-
-
-
-border-radius:20px;
-
-
-
-
-
-margin-top:20px;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-/* POSTER */
-
-
-
-
-
-.posterGrid{
-
-
-
-
-
-display:grid;
-
-
-
-
-
-grid-template-columns:
-
-
-
-repeat(auto-fit,minmax(300px,1fr));
-
-
-
-
-
-gap:25px;
-
-
-
-
-
-margin-top:40px;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-.poster{
-
-
-
-
-
-background:rgba(255,255,255,0.95);
-
-
-
-
-
-border-radius:20px;
-
-
-
-
-
-overflow:hidden;
-
-
-
-
-
-color:#0b4d2b;
-
-
-
-
-
-transition:0.4s;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-.poster:hover{
-
-
-
-
-
-transform:scale(1.03);
-
-
-
-}
-
-
-
-
-
-
-
-
-
-.poster img{
-
-
-
-
-
-width:100%;
-
-
-
-
-
-height:250px;
-
-
-
-
-
-object-fit:cover;
-
-
-
-}
-
-
-
-
-
-
-
-.posterText{
-
-
-
-
-
-padding:20px;
-
-
-
-}
-
-
-
-
-
-
-
-.posterText h3{
-
-
-
-
-
-margin-top:0;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-/* TIPS + FACTS + CHALLENGE */
-
-
-
-
-
-#tips,
-
-
-
-#facts,
-
-
-
-#challenge{
-
-
-
-
-
-padding:60px 20px;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-.tips-grid,
-
-
-
-.facts-grid{
-
-
-
-
-
-display:grid;
-
-
-
-
-
-grid-template-columns:
-
-
-
-repeat(auto-fit,minmax(250px,1fr));
-
-
-
-
-
-gap:20px;
-
-
-
-
-
-margin-top:30px;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-.tips-card,
-
-
-
-.fact-box,
-
-
-
-.challenge-box{
-
-
-
-
-
-background:rgba(255,255,255,0.92);
-
-
-
-
-
-color:#0b4d2b;
-
-
-
-
-
-padding:25px;
-
-
-
-
-
-border-radius:20px;
-
-
-
-
-
-
-
-box-shadow:
-
-
-
-0 8px 20px rgba(0,0,0,0.25);
-
-
-
-
-
-
-
-backdrop-filter:blur(5px);
-
-
-
-
-
-
-
-transition:0.3s;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-.tips-card:hover,
-
-
-
-.fact-box:hover,
-
-
-
-.challenge-box:hover{
-
-
-
-
-
-transform:
-
-
-
-translateY(-8px)
-
-
-
-scale(1.03);
-
-
-
-
-
-
-
-box-shadow:
-
-
-
-0 12px 25px rgba(0,0,0,0.35);
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-/* CHALLENGE */
-
-
-
-
-
-.challenge-box{
-
-
-
-
-
-max-width:500px;
-
-
-
-
-
-margin:auto;
-
-
-
-}
-
-
-
-
-
-
-
-.challenge-box h3{
-
-
-
-
-
-font-size:28px;
-
-
-
-}
-
-
-
-
-
-
-
-.challenge-box button{
-
-
-
-
-
-margin-top:20px;
-
-
-
-
-
-cursor:pointer;
-
-
-
-
-
-font-size:16px;
-
-
-
-
-
-transition:0.3s;
-
-
-
-}
-
-
-
-
-
-
-
-
-
-.challenge-box button:hover{
-
-
-
-
-
-background:#12a05a;
-
-
-
-
-
-transform:scale(1.05);
-
-
-
-}
-
-
-
-
-
-
-
-#challengeText{
-
-
-
-
-
-margin-top:20px;
-
-
-
-
-
-font-size:20px;
-
-
-
-
-
-font-weight:bold;
-
-
-
-
-
-color:#0b4d2b;
-
-
-
-}
-/* ANIMATION CARD MASUK */
-
-.card{
-  animation: fadeIn 1s ease;
-}
-
-
-@keyframes fadeIn{
-
-from{
-
-  opacity:0;
-
-  transform:translateY(20px);
-
-}
-
-
-to{
-
-  opacity:1;
-
-  transform:translateY(0);
-
-}
-
-}
-
-/* RESPONSIVE PHONE */
-
-@media(max-width:600px){
-
-header h1{
-
-font-size:35px;
-
-}
-
-
-header{
-
-padding:30px 10px;
-
-}
-
-
-nav a{
-
-display:block;
-
-margin:10px auto;
-
-width:200px;
-
-}
-
-
-section{
-
-padding:40px 10px;
-
-}
-
-
-.grid{
-
-grid-template-columns:1fr;
-
-}
-
-
-.box{
-
-width:80%;
-
-height:auto;
-
-}
-
-
-#map{
-
-height:350px;
-
-}
-
-
-.posterGrid{
-
-grid-template-columns:1fr;
-
-}
+  });
 
 }
